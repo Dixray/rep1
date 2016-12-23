9a1bae7b7c790a800240c6afa9dccf2406ac836a
Dixray <mitdi007@mail.ru>
2016-12-23 09:52:05 +0200

    build1

---
+++ Site/save.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$message = htmlspecialchars($_POST["message"]);
+
+		$link = mysqli_connect('localhost', 'root', '', 'basename') or die('Cant connect: ');
+		$query = "INSERT INTO `mysite` VALUES ('$name','$email','$tel','$message')";
+		mysqli_query($link, $query) or die('Cant query: ');
+		header("Location: file:index.html");
+?>
